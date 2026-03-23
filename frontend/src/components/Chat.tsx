b171814121857f32a8633f716dae7cdd1bac7c10
lauralee01 <lauralee285@yahoo.com>
2026-03-23 13:11:36 -0500

    added fixed chat input field in the chat UI

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -43,7 +43,7 @@ export function Chat() {
   };
 
   return (
-    <div className="flex h-[100dvh]text-zinc-900 dark:text-zinc-100">
+    <div className="flex h-[100dvh] min-h-0 bg-zinc-50 text-zinc-900 dark:bg-zinc-950 dark:text-zinc-100">
       <ChatDesktopSidebar {...sidebarProps} />
 
       <ChatMobileDrawer
@@ -52,7 +52,7 @@ export function Chat() {
         {...sidebarProps}
       />
 
-      <div className="flex min-w-0 flex-1 flex-col">
+      <div className="flex min-h-0 min-w-0 flex-1 flex-col overflow-hidden">
         <MobileChatHeader onOpenSidebar={() => setSidebarOpen(true)} />
 
         <ChatMessageList
--- frontend/src/components/chat-ui/ChatDesktopSidebar.tsx
+++ frontend/src/components/chat-ui/ChatDesktopSidebar.tsx
@@ -12,8 +12,8 @@ type Props = {
 
 export function ChatDesktopSidebar(props: Props) {
   return (
-    <aside className="hidden w-[min(100%,18rem)] shrink-0 flex-col bg-[#181818] md:flex">
-      <div className=" px-4 py-3 d">
+    <aside className="hidden h-full min-h-0 w-[min(100%,18rem)] shrink-0 flex-col border-r border-zinc-800/80 bg-[#181818] md:flex">
+      <div className="border-b border-zinc-800/80 px-4 py-3">
         <BrandLockup size="md" />
       </div>
       <SidebarConversationList {...props} />
